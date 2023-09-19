bc907cf3b5a077583e474aab74b0d03749f2bfcc
arungit2020 <aruprajapati9825@gmail.com>
2023-09-19 10:13:00 +0530

    then vs await

--- async-await/intro.js
+++ async-await/intro.js
@@ -2,8 +2,48 @@
 async function getUsers() {
 	return "Hello World!"
 }
-const data=getUsers()
-console.log(data) // async function always returns a promise we will get promise as pending 
-getUsers().then(res => {
-	console.log(res)
-})
+// const data=getUsers()
+// console.log(data) // async function always returns a promise we will get promise as pending 
+// getUsers().then(res => {
+// 	console.log(res)
+// })
+
+
+//async await 
+function myPromiseFunction_async() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Success');
+        }, 2000);
+    });
+}
+
+// (async () => {
+//     console.log('Start');
+//     const result = await myPromiseFunction_async();
+//     console.log(`result - ${result}`);
+//     console.log('End');
+// })();
+//output
+//start
+//result-success
+//End
+//same example with then 
+function myPromiseFunction_then() {
+	return new Promise((res, rej) => {
+		   setTimeout(() => {
+            res('Success');
+        }, 2000);
+	})
+}
+(async () => {
+	console.log("Start");
+	myPromiseFunction_then().then(result => {
+		console.log(`result - ${result}`);
+	});
+	console.log("End");
+})();
+//output 
+// Start
+// End
+// result - Success
